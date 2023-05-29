9992039f2341ccc5d70655a7a3abdb5f790deb27
Hitalo <hitalo.emanoel@gmail.com>
2023-05-29 16:43:42 -0300

    adiciona SwapNodesPairs

---
+++ src/list/SwapNodesPairs.java
@@ -0,0 +1,36 @@
+package list;
+
+import utils.ListNode;
+
+public class SwapNodesPairs {
+	
+	public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null) return head;
+        
+        ListNode temp = head.next;
+        head.next = swapPairs(temp.next);
+        temp.next = head;
+        return temp;
+    }
+
+	public static void main(String[] args) {
+		SwapNodesPairs solution = new SwapNodesPairs();
+		System.out.println("Expected: 2 1 4 3");
+		System.out.print("Result:   ");
+		showValues(solution.swapPairs(createList()));
+	}
+	
+	
+	private static ListNode createList() {
+		return new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, null))));
+	}
+	
+	
+	private static void showValues(ListNode head) {
+		while(head != null) {
+			System.out.print(head.val + " ");
+			head = head.next;
+		}
+	}
+
+}
